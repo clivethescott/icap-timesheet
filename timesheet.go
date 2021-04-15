32c9e8eefe1b51cc639a3804cb226be20ce85e25
Clive Gurure <clivethescott@gmail.com>
2021-04-15 21:32:38 +0200

    error handling updates

--- timesheet.go
+++ timesheet.go
@@ -11,13 +11,15 @@ import (
 )
 
 const (
-	submitterNameCell = "A44"
-	dateCell          = "A45"
-	monthBeginCol     = 'D'
-	dateFormat        = "02-01-2006"
-	monthFormat       = "Jan"
-	dayOfMonthFormat  = "02/01"
-	daysEarned        = "2.5"
+	submitterNameCell       = "A44"
+	dateCell                = "A45"
+	monthBeginCol           = 'D'
+	dateFormat              = "02-01-2006"
+	generatedFileDateFormat = "02-Jan-2006"
+	monthFormat             = "Jan"
+	dayOfMonthFormat        = "02/01"
+	daysEarned              = "2.5"
+	outputDir               = "gen/"
 )
 
 const (
@@ -27,35 +29,41 @@ const (
 	supervisorInitialsRow = 43
 	dayOfMonthRow         = 44
 	daysEarnedRow         = 47
+	newBalanceRow         = 49
 )
 
 var sheet string
 
-var file = flag.String("file", "timesheet.xlsx", "Timesheet file")
+var template = flag.String("file", "timesheet.xlsx", "Timesheet file")
 var submitter = flag.String("name", "C. Gurure", "Submitter Name")
 var initials = flag.String("initials", "CG", "Submitter Initials")
 var supervisorInitials = flag.String("sinitials", "TD", "Supervisor Initials")
-var sheetIndex = flag.Int("sheet", 0, "Sheet index")
+var sheetIndex = flag.Int("sheet", 0, "0-based Sheet index")
 var month = flag.Int("month", int(time.Now().Month()), "1-based Month number")
-var year = flag.Int("year", int(time.Now().Year()), "Year")
+var year = flag.Int("year", time.Now().Year(), "Year")
 
 func lastDayOfMonth(fromDate time.Time) time.Time {
 	return time.Date(fromDate.Year(), fromDate.Month()+1, 0, 0, 0, 0, 0, time.UTC)
 }
 
+func statFile(f string) error {
+	_, err := os.Stat(f)
+	return err
+}
+
 func openSheet() (*excelize.File, error) {
-	if _, err := os.Stat(*file); errors.Is(err, os.ErrNotExist) {
-		return nil, fmt.Errorf("problem reading input file: %v\n", err)
+	if err := statFile(*template); err != nil {
+		return nil, fmt.Errorf("input template unreadable: %v", err)
 	}
-	f, err := excelize.OpenFile(*file)
+	f, err := excelize.OpenFile(*template)
 	if err != nil {
-		return nil, fmt.Errorf("failed to read input file: %v\n", err)
+		return nil, fmt.Errorf("failed to read input template: %v\n", err)
 	}
 	return f, nil
 }
 
-func saveSheet(f *excelize.File, submissionDate string) error {
-	updatedFile := fmt.Sprintf("gen/timesheet-%s.xlsx", submissionDate)
+func saveSheet(f *excelize.File, submissionDate time.Time) error {
+	updatedFile := fmt.Sprintf("%stimesheet-%s.xlsx", outputDir, submissionDate.Format(generatedFileDateFormat))
 	return f.SaveAs(updatedFile)
 }
 
@@ -80,8 +88,10 @@ func currentMonthCol(f *excelize.File) (string, error) {
 			return "", err
 		}
 
-		daysEarnedCell := cell(col, daysEarnedRow)
-		f.SetCellValue(sheet, daysEarnedCell, daysEarned)
+		daysErr := updateDaysEarned(f, col)
+		if daysErr != nil {
+			return "", fmt.Errorf("failed to update days earned: %v", daysErr)
+		}
 
 		if monthName == currentMonthName {
 			return col, nil
@@ -93,12 +103,28 @@ func currentMonthCol(f *excelize.File) (string, error) {
 func cell(col string, row int) string {
 	return fmt.Sprintf("%s%d", col, row)
 }
-func updateCol(f *excelize.File, col string) {
+
+func updateDaysEarned(f *excelize.File, col string) error {
+	daysEarnedCell := cell(col, daysEarnedRow)
+	f.SetCellValue(sheet, daysEarnedCell, daysEarned)
+
+	newBalanceCell := cell(col, newBalanceRow)
+	res, err := f.CalcCellValue(sheet, newBalanceCell)
+	if err != nil {
+		return fmt.Errorf("failed to update days earned: %v", err)
+	}
+
+	f.SetCellValue(sheet, newBalanceCell, res)
+	return nil
+}
+
+func updateCol(f *excelize.File, col string) error {
 
 	f.SetCellValue(sheet, cell(col, initialsRow), *initials)
 	f.SetCellValue(sheet, cell(col, supervisorInitialsRow), *supervisorInitials)
 	f.SetCellValue(sheet, cell(col, dayOfMonthRow), lastDayOfMonth(currentTime()).Format(dayOfMonthFormat))
 	f.SetCellValue(sheet, cell(col, daysEarnedRow), daysEarned)
+	return updateDaysEarned(f, col)
 }
 
 func updateValues(f *excelize.File, submissionDate string) error {
@@ -108,12 +134,16 @@ func updateValues(f *excelize.File, submissionDate string) error {
 	if err != nil {
 		return fmt.Errorf("failed to update values: %v", err)
 	}
-	updateCol(f, col)
-	return nil
+	return updateCol(f, col)
 }
 
 func main() {
 	flag.Parse()
+
+	if err := statFile(outputDir); err != nil {
+		fmt.Printf("output dir %s unreadable: %v\n", outputDir, err)
+		os.Exit(1)
+	}
 
 	f, err := openSheet()
 	if err != nil {
@@ -122,12 +152,12 @@ func main() {
 	}
 
 	sheet = f.GetSheetName(*sheetIndex)
-	submissionDate := lastDayOfMonth(currentTime()).Format(dateFormat)
-	if err := updateValues(f, submissionDate); err != nil {
-		fmt.Printf("failed to update values: %v", err)
+	submissionDate := lastDayOfMonth(currentTime())
+	if err := updateValues(f, submissionDate.Format(dateFormat)); err != nil {
+		fmt.Printf("failed to update values: %v\n", err)
 	}
 	if err := saveSheet(f, submissionDate); err != nil {
-		fmt.Printf("failed to create updated timesheet: %v", err)
+		fmt.Printf("failed to create updated timesheet: %v\n", err)
 	}
 
 }
